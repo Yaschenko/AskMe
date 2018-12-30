302465efb35cfd8fa02c6ce0eb3ced21d604ac3d
Yaschenko <yura.yaschenko@gmail.com>
2018-12-30 17:26:40 +0200

    updated flow

--- README.md
+++ README.md
@@ -4,31 +4,38 @@
 ## Description:
 
 ## Rinkeby Address:
-### 0xad9acc7a65153d7f757fcc482f3c5d2fe4b405b3
-### 0x82081931c62080b04462b0ba89ba43500cc0f2fa - old
+### 0xd5de5a14fa28c527d9054105813a9eba71a1a3b9
+- fee: 1% 
+- manager address: 0x115bCe73559ffcbd5A6374C988B8502C90E17273
+### 0xad9acc7a65153d7f757fcc482f3c5d2fe4b405b3 - deprecated
+### 0x82081931c62080b04462b0ba89ba43500cc0f2fa - deprecated
 
 ## Main Net Address:
 
 ## API:
 ### askQuestion
-- fn: askQuestion(string reciver, string question, string configs) public payable
+- fn: askQuestion(address reciver, string question, string configs) public payable
 - params:
-  - reciver : string
+  - reciver : address
   - question : string
   - configs : string(optional)
   
 ### answerQuestion
-- fn: answerQuestion(uint id, string sender, address user, address payoutAddress, string text, string config) public onlyManager
+- fn: answerQuestion(uint id, string text, string config) public onlyManager
 - params:
   - id : uint
-  - sender : string
-  - user : address
-  - payoutAddress : address - who should get money for answer
+  - text : string
+  - config : string(optional)
+
+### freeAnswerQuestion
+- fn: freeAnswerQuestion(uint id, string text, string config) public onlyManager
+- params:
+  - id : uint
   - text : string
   - config : string(optional)
   
 ### rejectQuestion
-- fn: rejectQuestion(uint id, string sender, address user) public onlyManager
+- fn: rejectQuestion(uint id) public onlyManager
 - params:
   - id : uint
   - sender : string
@@ -40,8 +47,10 @@
 - params:
   - id : uint
 
-  
+
 ## Events
-### DidSendQuestionEvent(uint id, address sender, string reciver, string question, string configs, uint payout);
-### DidAswerQuestionEvent(uint id, string sender, address reciver, address payoutAddress, string answer, string configs);
-### DidRejectQuestionEvent(uint id, string sender, address user);
+### event DidSendQuestionEvent(uint id, address sender, address reciver, string question, string configs, uint payout);
+### event DidAswerQuestionEvent(uint id, address reciver, address sender, uint payout, string answer, string configs);
+### event DidFreeAswerQuestionEvent(uint id, address reciver, address sender, uint payout, string answer, string configs);
+### event DidRejectQuestionEvent(uint id, address reciver, address sender);
+### event DonatedQuestion(uint id, address user, uint amount);
--- contracts/AskMe.sol
+++ contracts/AskMe.sol
@@ -7,17 +7,24 @@ contract AskMe is Ownable {
     //libs
     using SafeMath for uint;
     //events
-    event DidSendQuestionEvent(uint id, address sender, string reciver, string question, string configs, uint payout);
-    event DidAswerQuestionEvent(uint id, string sender, address reciver, address payoutAddress, string answer, string configs);
-    event DidRejectQuestionEvent(uint id, string sender, address user);
+    event DidSendQuestionEvent(uint id, address sender, address reciver, string question, string configs, uint payout);
+    event DidAswerQuestionEvent(uint id, address reciver, address sender, uint payout, string answer, string configs);
+    event DidFreeAswerQuestionEvent(uint id, address reciver, address sender, uint payout, string answer, string configs);
+    event DidRejectQuestionEvent(uint id, address reciver, address sender);
     event DonatedQuestion(uint id, address user, uint amount);
     //
+    struct QuestionObject {
+        address reciver;
+        address sender;
+        uint payout;
+    }
     address private manager;
-    uint private questionsCount;
-    uint private fee = 0.0 ether;
+    // uint private questionsCount;
+    uint private minimumFee = 0.0 ether;
+    uint private fee = 1000;//1000 == 1%
     uint private minimumProfit = 0.0 ether;
-    // mapping(uint => uint) private payouts;
-    uint[] private payouts;
+    
+    QuestionObject[] private questions;
     constructor() public {
         // manager = msg.sender;
         manager = 0x115bCe73559ffcbd5A6374C988B8502C90E17273;
@@ -27,7 +34,6 @@ contract AskMe is Ownable {
         require(msg.sender == manager);
         _;
     }
-
 
     function () public payable {
         owner.transfer(msg.value);
@@ -56,35 +62,68 @@ contract AskMe is Ownable {
     function setMinimumProfit(uint profit) public onlyOwner {
         minimumProfit = profit;
     }
+    function getMinimumFee() public view returns(uint) {
+        return minimumFee;
+    }
+    function setMinimumFee(uint minFee) public onlyOwner {
+        minimumFee = minFee;
+    }
     //
-    function askQuestion(string reciver, string question, string configs) public payable returns(uint) {
-        require(msg.value > fee.add(minimumProfit));
-        owner.transfer(fee);
-        uint index = payouts.push(msg.value.sub(fee));
-        emit DidSendQuestionEvent(index, msg.sender, reciver, question, configs, payouts[index]);
+    function calculateFee(uint value) private view returns(uint) {
+        uint feeValue = value.mul(fee).div(1000);
+        if (feeValue > minimumFee) {
+            return feeValue;
+        } else {
+            return minimumFee;
+        }
+    }
+
+    function askQuestion(address reciver, string question, string configs) public payable returns(uint) {
+        uint feeValue = calculateFee(msg.value);
+        require(msg.value >= feeValue.add(minimumProfit));
+        owner.transfer(feeValue);
+        uint payout = msg.value.sub(feeValue);
+        QuestionObject memory questionData = QuestionObject(reciver, msg.sender, payout);
+        uint index = questions.push(questionData);
+        emit DidSendQuestionEvent(index, msg.sender, reciver, question, configs, payout);
         return index;
     }
 
-    function answerQuestion(uint id, string sender, address user, address payoutAddress, string text, string config) public onlyManager {
-        require(payouts[id] > 0);
-        uint payout = payouts[id];
-        payouts[id] = 0;
-        payoutAddress.transfer(payout);
-        emit DidAswerQuestionEvent(id, sender, user, payoutAddress, text, config);
+    function answerQuestion(uint id, string text, string config) public onlyManager {
+        QuestionObject storage questionData = questions[id];
+        require(questionData.payout > 0);
+        uint payout = questionData.payout;
+        questionData.payout = 0;
+        
+        questionData.reciver.transfer(payout);
+        emit DidAswerQuestionEvent(id, questionData.reciver, questionData.sender, payout, text, config);
     }
-    function rejectQuestion(uint id, string sender, address user) public onlyManager {
-        require(payouts[id] > 0);
-        uint payout = payouts[id];
-        payouts[id] = 0;
-        user.transfer(payout);
-        emit DidRejectQuestionEvent(id, sender, user);
+
+    function freeAnswerQuestion(uint id, string text, string config) public onlyManager {
+        QuestionObject storage questionData = questions[id];
+        require(questionData.payout > 0);
+        uint payout = questionData.payout;
+        questionData.payout = 0;
+        
+        questionData.sender.transfer(payout);
+        emit DidFreeAswerQuestionEvent(id, questionData.reciver, questionData.sender, payout, text, config);
+    }
+    function rejectQuestion(uint id) public onlyManager {
+        QuestionObject storage questionData = questions[id];
+        require(questionData.payout > 0);
+        uint payout = questionData.payout;
+        questionData.payout = 0;
+        questionData.sender.transfer(payout);
+        emit DidRejectQuestionEvent(id, questionData.sender, questionData.reciver);
     }
     function donateQuestion(uint id) public payable {
-        require(msg.value > fee);
-        require(payouts[id] > 0);
+        uint feeValue = calculateFee(msg.value);
+        require(msg.value > feeValue);
+        QuestionObject storage questionData = questions[id];
+        require(questionData.payout > 0);
         owner.transfer(fee);
-        uint userProfit = msg.value.sub(fee);
-        payouts[id] = payouts[id].add(userProfit);
+        uint userProfit = msg.value.sub(feeValue);
+        questionData.payout = questionData.payout.add(userProfit);
         emit DonatedQuestion(id, msg.sender, userProfit);
     }
     // function cancelQuestion()
